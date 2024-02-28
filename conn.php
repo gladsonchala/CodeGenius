34d00f4faff68ded21fa9baf1813f1f0985725a0
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2024-02-28 23:09:02 +0300

    Initial commit

---
+++ conn.php
@@ -0,0 +1,14 @@
+$servername = 'localhost:3308';
+$username = 'root';
+$password = '';
+$dbname = 'CodeGenius';
+$is_db_connected = false;
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $is_db_connected = true;
+} catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+    $is_db_connected = false;
+}
